e1a06051ccb42ffbb3d567cee85c582f879fabf9
stricker <liuchen195@163.com>
2016-01-22 16:45:23 +0800

    asdfsafsa

--- sdasdasdasd 2 2/sdasdasdasd/Classes/Home/HomeViewController.m
+++ sdasdasdasd 2 2/sdasdasdasd/Classes/Home/HomeViewController.m
@@ -704,6 +704,9 @@
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
     
     NSString *url = request.URL.absoluteString;
+    if ([url isEqualToString:@"about:blank"]) {
+        return NO;
+    }
     if (webView.tag == 100) {
         
         if ([url rangeOfString:@"/UserCenter/Login.aspx"].location !=  NSNotFound) {
