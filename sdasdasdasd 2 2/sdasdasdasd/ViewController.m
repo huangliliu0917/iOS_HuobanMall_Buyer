d39b5e8489bc0a270b9dd831085d1636d60a5105
stricker <liuchen195@163.com>
2016-06-15 10:24:35 +0800

    修改保存

--- sdasdasdasd 2 2/sdasdasdasd/ViewController.m
+++ sdasdasdasd 2 2/sdasdasdasd/ViewController.m
@@ -45,7 +45,7 @@
         [shareParams SSDKSetupShareParamsByText:nil
                                          images:imageArray
                                             url:[NSURL URLWithString:@"www.baidu.com"]
-                                          title:@"行装"
+                                          title:@"乡村来"
                                            type:SSDKContentTypeAuto];
         //2、分享（可以弹出我们的分享菜单和编辑界面）
         [ShareSDK showShareActionSheet:nil //要显示菜单的视图, iPad版中此参数作为弹出菜单的参照视图，只有传这个才可以弹出我们的分享菜单，可以传分享的按钮对象或者自己创建小的view 对象，iPhone可以传nil不会影响
